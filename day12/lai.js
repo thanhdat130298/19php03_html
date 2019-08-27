36a8862e72ca62eeb39adcb7a8b3054382639f29
thanhdat130298 <nguyenthanhdat1302@gmail.com>
2019-08-27 18:09:04 +0700

    Day14

--- day12/lai.js
+++
@@ -1,7 +0,0 @@
-var lai=0.6/100;
-var goc=20000000;
-var tong;
-for(var i = 1; i <= 36; i++){
-	tong=goc*lai;
-}
-document.write("A nhận được " + (tong+goc) + "VND sau 3 năm gửi 2 chục củ");
---
+++ day14/max.js
@@ -0,0 +1,10 @@
+var a = prompt("Nhap a: ");
+var b = prompt("Nhap b: ");
+var c = prompt("Nhap c: ");
+var max=a;
+parseInt(a);
+parseInt(b);
+parseInt(c);
+if (b>max) max=b;
+if (c>max) max=c;
+document.write("Max la: " + max);
