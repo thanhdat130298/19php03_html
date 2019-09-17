de32450b6a06334d3ed7002c0770faa860ac9de4
thanhdat130298 <nguyenthanhdat1302@gmail.com>
2019-09-17 18:02:04 +0700

    Tinh tien dien

--- tinhtien/tinh.js
+++ tinhtien/tinh.js
@@ -5,30 +5,68 @@ var lM=document.getElementById('lastM').value;
 var lDay=document.getElementById('lastDay').value;
 var fDay=document.getElementById('firstDay').value;
 var name=document.getElementById('name').value;
-	if(name==''){
-		 document.getElementById('errorName')="Khong duoc rong";
+var d1 = Date.parse(lDay);
+var d2 = Date.parse(fDay);
+	if((name=='')||(!isNaN(name))){
+		 document.getElementById('errorName').innerHTML ="Tên không được rỗng hoặc số!";
 		 valueIN=false;
 	}
- 	else if (fM==''){
-        document.getElementById("errorFirsM").innerHTML = "Chỉ nhập giá trị số.";
+ 	if ((fM=='')){
+        document.getElementById("errorFirstM").innerHTML = "Chưa nhập số điện đầu kì!";
  		valueIN=false;
  	}
- 	else if (lM==''){
- 		document.getElementById('errorLastM')="Khong duoc rong";
+ 	if (isNaN(fM)) {
+ 		document.getElementById("errorFirstM").innerHTML = "Phải là số!";
+ 		valueIN=false;	
+ 	}
+ 	if (isNaN(lM)) {
+ 		document.getElementById("errorLastM").innerHTML = "Phải là số!";
+ 		valueIN=false;	
+ 	}
+ 	if ((lM=='')){
+ 		document.getElementById('errorLastM').innerHTML ="Chưa nhập số điện cuối kì";
  		valueIN=false;
  	}
- 	else if (fM==''){
- 		document.getElementById('errorFirstM')="Khong duoc rong";
+ 	if ((!isNaN(lM))&&(!isNaN(fM))&&(lM<fM)) {
+ 		document.getElementById('errorNum').innerHTML="Số điện cuối kì phải lớn hơn số điện đầu kì";
  		valueIN=false;
  	}
- 	else if (lM==''){
- 		document.getElementById('errorLastM')="Khong duoc rong";
+ 	if (fM==''){
+ 		document.getElementById('errorFirstDay').innerHTML ="Chưa nhập ngày đầu kì";
  		valueIN=false;
  	}
+ 	if (lM==''){
+ 		document.getElementById('errorLastDay').innerHTML ="Chưa nhập ngày cuối kì";
+ 		valueIN=false;
+ 	}
+ 	if(d2>d1) {
+ 		valueIN=false;
+ 		document.getElementById('errorDay').innerHTML="Ngày phải đúng !";
+ 	}
+ 	var n=lM-fM;
+ 	console.log(n);
+ 	var a=0,b=0,c=0;
  	if(valueIN==true){
- 		if((lastM-firstM)<=100) alert((lastM-firstM)*1500);
- 		else if( ( (lastM-firstM)>100)&&(lastM-firstM)<=200) alert((lastM-firstM)*2000);
- 		else if(((lastM-firstM)>200)) alert((lastM-firstM)*3500);
+ 		if(n<=100){
+ 			a=n*1500;
+ 		}
+ 		if((n<=300)&&(n>100)){
+ 			a=100*1500;
+ 			b=(n-100)*2000;
+ 		}
+ 		if(n>=300){
+ 			a=100*1500;
+ 			b=200*2000;
+ 			c=(n-300)*3500;
+ 		}
+	document.getElementById('result').innerHTML=a+b+c;
+	var result=a+b+c;
+	document.write("<h3>Hóa đơn tiền điện của bạn:</h3><br>");
+	document.write("Tên: "+name+"<br>");
+	document.write("Hóa đơn từ ngày: "+fDay+" đến ngày "+lDay+"<br>");
+	document.write("Sử dụng: "+n+" số<br>");
+	document.write("Tổng thanh toán: "+result);
  	}
+ 	
 
 }
