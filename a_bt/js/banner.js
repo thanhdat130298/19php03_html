0eeb689ab4f4e25f353bb141039fcf3b7d6e57a0
thanhdat130298 <nguyenthanhdat1302@gmail.com>
2019-10-17 18:18:35 +0700

    1/2

---
+++ a_bt/js/banner.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$("#left").click(function(){
+	$('.hinh').hide(0).show(1000);
+	$('.cungcap').hide(0).fadeIn(2000);
+});
+$("#right").click(function(){
+	$('.hinh').hide(0).show(1000);
+
+	$('.cungcap').hide(0).fadeIn(2000);
+});
+});
